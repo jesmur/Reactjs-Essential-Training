f01f48f5fbf22f61346a9524c7fc8ff3316a6b9f
jesmur <jessicamurray91@gmail.com>
2019-03-25 18:51:29 -0300

    chapter 4_05 passing state as props

--- src/index.js
+++ src/index.js
@@ -8,35 +8,44 @@ let bookList = [
     {"title":"Cat's Cradle", "author":"Kurt Vonnegut", pages: 304}
 ]
 
-const Book = ({title, author, pages}) => {
+const Book = ({title, author, pages, freeBookmark}) => {
     return (
         <section>
             <h2>{title}</h2>
             <p>By: {author}</p>
             <p>Pages: {pages} pages</p>
+            <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
         </section>
     )
 }
 
 class Library extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            open: true
-        }
+
+    state = { 
+        open: true, 
+        freeBookmark: false 
     }
+
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open
+        }))
+    }
+
     render (){
         const { books } = this.props;
         return(
             <div>
                 <h1>The Library is {this.state.open ? 'open' : 'closed'}</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(
                     (book,i) => 
                     <Book 
                         key={i}
                         title={book.title} 
                         author={book.author} 
-                        pages={book.pages} />
+                        pages={book.pages} 
+                        freeBookmark={this.state.freeBookmark}/>
                 )}
             </div>
         )
